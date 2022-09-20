44cc783bce86d81844766f937c18b4f7325af167
Jonathan <Jonathan.Morse@la.gov>
2022-09-20 17:20:37 -0500

    Main Page and Create Page Edited
    Added more code to the main page. Created the create page and coded the UI for the create page.

---
+++ CaseMgmtPortal/CaseMgmtPortal/Controllers/CreateCaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaseMgmtPortal.Controllers
+{
+    public class CreateCaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CaseMgmtPortal/CaseMgmtPortal/Views/CreateCase/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CaseMgmtPortal.Views.CreateCase
+{
+    public class ReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
